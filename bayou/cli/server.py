b5e22e8d44bdea044dbc17ae4caa34ecf3db955a
Joel Watts <joel@joelwatts.com>
2015-03-22 09:52:42 -0500

    Separated CLI from server implementation.

---
+++ bayou/cli/server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import asyncio
+import logging
+
+import click
+
+from .. import server
+
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option('--logging', '-l', default="WARNING", help="Log level", show_default=True)
+@click.option('--address', '-a', default="127.0.0.1", help="Server address", show_default=True)
+@click.option('--port', '-p', default=4430, help="Server port", show_default=True)
+@click.option('--data', '-d', default=".", help="Data directory", show_default=True)
+def main(**options):
+    logging.basicConfig(level=getattr(logging, options['logging'].upper()))
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        server.start(
+            options['data'],
+            options['address'],
+            options['port'],
+            loop=loop
+        )
+    )
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ bayou/server.py
@@ -0,0 +1,145 @@
+import asyncio
+import datetime
+import logging
+import os
+
+from aiohttp import web
+
+import simplejson
+
+
+ENCODING = "UTF-8"
+
+
+logger = logging.getLogger(__name__)
+
+
+class Stream:
+    def __init__(self, root):
+        self.root = root
+        self._file = None
+        self._futures = []
+
+    def __repr__(self):
+        return "{0.__name__}({1.root!r})".format(type(self), self)
+
+    def _notify(self, text, offset):
+        futures = self._futures
+        result = (text, offset)
+        for future in futures:
+            if future.done():
+                continue
+            future.set_result(result)
+        futures.clear()
+
+    def _open(self):
+        file = self._file
+        if file is None:
+            root = self.root
+            if not os.path.exists(root):
+                os.makedirs(root)
+            file = open(os.path.join(root, "0.ldjson"), 'a+')
+            self._file = file
+        return file
+
+    def append(self, data):
+        file = self._open()
+        file.seek(0, os.SEEK_END)
+        obj = dict(
+            data=data,
+            offset=file.tell(),
+            time="{}Z".format(datetime.datetime.utcnow().isoformat())
+        )
+        text = "{}\n".format(simplejson.dumps(obj, separators=(',', ':'), sort_keys=True))
+        file.write(text)
+        file.flush()
+        self._notify(text, file.tell())
+        return text
+
+    def next_offset(self, since_offset=None):
+        if since_offset is None:
+            return 0
+        file = self._open()
+        file.seek(since_offset)
+        file.readline()
+        return file.tell()
+
+    @asyncio.coroutine
+    def read(self, offset, loop=None):
+        future = asyncio.Future(loop=loop)
+        file = self._open()
+        file.seek(offset)
+        text = file.readline()
+        if text:
+            future.set_result((text, file.tell()))
+        else:
+            self._futures.append(future)
+        return future
+
+
+class Handler:
+    def __init__(self, root):
+        self.root = root
+        self._streams = {}
+
+    def stream(self, name):
+        cache = self._streams
+        try:
+            stream = cache[name]
+        except KeyError:
+            stream = Stream(os.path.join(self.root, name))
+            cache[name] = stream
+        return stream
+
+    @asyncio.coroutine
+    def append(self, request):
+        stream = self.stream(request.match_info['name'])
+        try:
+            data = yield from request.json(loader=simplejson.loads)
+        except simplejson.JSONDecodeError:
+            raise web.HTTPBadRequest
+        text = stream.append(data)
+        return web.Response(
+            body=text.encode(ENCODING),
+            content_type="application/json; charset={}".format(ENCODING)
+        )
+
+    @asyncio.coroutine
+    def read(self, request):
+        stream = self.stream(request.match_info['name'])
+
+        response = web.StreamResponse()
+        response.content_type = "application/ldjson; charset={}".format(ENCODING)
+        response.start(request)
+
+        loop = request.app.loop
+
+        try:
+            since_offset = int(request.GET['since'])
+        except KeyError:
+            since_offset = None
+        except ValueError:
+            raise web.HTTPBadRequest
+
+        next_offset = stream.next_offset(since_offset)
+        while True:
+            text, next_offset = yield from stream.read(next_offset, loop=loop)
+            response.write(text.encode(ENCODING))
+            yield from response.drain()
+
+        yield from response.write_eof()
+        return response
+
+
+@asyncio.coroutine
+def start(root, address, port, loop=None):
+    if loop is None:
+        loop = asyncio.get_event_loop()
+
+    handler = Handler(root)
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/{name}', handler.read)
+    app.router.add_route('POST', '/{name}', handler.append)
+
+    server = yield from loop.create_server(app.make_handler(), address, port)
+    return server
